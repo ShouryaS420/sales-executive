69d6ad2b9e97c63687b3258ea4214731f7691a70
ShouryaS420 <shouryanarwade37@gmail.com>
2024-01-25 12:43:56 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "./routers/User.js";
 import Excel from "./routers/Excel.js";
+import Project from "./routers/Project.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -13,3 +14,4 @@ app.use(cors());
 
 app.use("/api/user", User);
 app.use("/api/excel", Excel);
+app.use("/api/project", Project);
